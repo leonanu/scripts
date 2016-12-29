217ed261b73667eb65893ed37d5934e04fa31acc
leonanu <nanu@inanu.net>
2016-12-29 13:43:45 +0800

    update

--- pip_update.sh
+++ pip_update.sh
@@ -2,6 +2,11 @@
 
 PIP_BIN=/usr/local/python2.7/bin/pip
 
+if [ ! -x ${PIP_BIN} ];then
+    echo "${PIP_BIN}: Not exist or not executable!"
+    exit 1
+fi
+
 pkgs=$(${PIP_BIN} list --format=legacy | cut -d ' ' -f 1)
 
 for pkg in ${pkgs};
