6e35a730024b25179221b05eb6c9657f675440c2
leonanu <nanu@inanu.net>
2017-01-07 22:55:25 +0800

    update

--- no_repeat_random.py
+++ no_repeat_random.py
@@ -4,19 +4,27 @@
 import random
 
 
-RND_RANGE = 1000000
+LIMITATION = 1000000
 
 count = int(raw_input('How many random integer number you want? '))
 
 lst_random = []
 
-for i in range(count):
-    value = random.randint(0, RND_RANGE)
+if count == LIMITATION:
+    for i in range(0, count):
+        lst_random.append(i)
 
-    while value in lst_random:
-        value = random.randint(0, RND_RANGE)
+elif count < LIMITATION:
+    for i in range(count):
+        value = random.randint(0, LIMITATION)
 
-    lst_random.append(value)
+        while value in lst_random:
+            value = random.randint(0, LIMITATION)
 
+        lst_random.append(value)
+
+else:
+    error = '\nError!\nYour want %d random number, but the random number LIMITATION is %d.' % (count, LIMITATION)
+    raise SystemExit(error)
 
 print lst_random
