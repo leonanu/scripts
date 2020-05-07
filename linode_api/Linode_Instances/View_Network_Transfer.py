6f9c1e7e885524600573cb8afc46fb93c0ab14fc
leonanu <nanu@inanu.net>
2020-05-07 12:25:10 +0800

    update View_Network_Transfer.py

--- linode_api/Linode_Instances/View_Network_Transfer.py
+++ linode_api/Linode_Instances/View_Network_Transfer.py
@@ -5,16 +5,17 @@ import json
 import sys
 from urllib import request
  
-
+LINODE_API_VER = 'v4'
 LINODE_ID = ''
 LINODE_TOKEN_RO = ''
  
 
 def getTransfer(linode_id, argv):
-    url = 'https://api.linode.com/v4/linode/instances/' + linode_id + '/transfer'
+    url = 'https://api.linode.com/' + LINODE_API_VER + '/linode/instances/' + linode_id + '/transfer'
     header_dict = {
         'User-Agent': 'User-Agent: curl/7.68.0',
-        'Authorization': 'Bearer ' + LINODE_TOKEN_RO}
+        'Authorization': 'Bearer ' + LINODE_TOKEN_RO
+    }
  
     req = request.Request(url, headers=header_dict)
     res = request.urlopen(req, timeout=10)
