52de60835f111987ad2b2f3ced36e77ec8514c8d
leonanu <nanu@inanu.net>
2020-05-28 17:15:32 +0800

    add Reboot_Linode.py

---
+++ linode_api/Linode_Instances/Reboot_Linode.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import json
+from urllib import request
+ 
+LINODE_API_VER = 'v4'
+LINODE_ID = ''
+LINODE_TOKEN_RW = ''
+ 
+
+def reboot(linode_id):
+    url = 'https://api.linode.com/' + LINODE_API_VER + '/linode/instances/' + linode_id + '/reboot'
+    header_dict = {
+        'User-Agent': 'curl/7.68.0',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + LINODE_TOKEN_RW
+    }
+ 
+    req = request.Request(url, headers=header_dict, method='POST')
+    res = request.urlopen(req, timeout=10)
+    ret = res.read()
+ 
+    jsonData = json.loads(ret)
+ 
+    print(jsonData)
+ 
+
+def main():
+    reboot(LINODE_ID)
+ 
+
+if __name__ == '__main__':
+    main()
